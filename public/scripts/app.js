48a057573af0e514195e13e197e5f4ffb57a0a21
Geoff Fox <fox.geoffrey@gmail.com>
2021-01-12 13:13:43 -0800

    Add: Simulation babel.js pre-compiler

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,8 +1,15 @@
 console.log(`app.js working!`);
 
 //JSX - JavaScript XML
+//var template = <p>This is JSX from app.js!</p>;
 
-var template = <p>This is JSX from app.js!</p>;
+//The result from using Babeljs
+var template = React.createElement(
+    "p",
+    {id: "someId"},
+    "This is JSX from app.js"
+);
+
 var appRoot = document.getElementById('app');
 
 ReactDOM.render(template, appRoot);
