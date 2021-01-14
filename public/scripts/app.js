5cbb31c03c27c516981ae9c596dca677daf37d1c
Geoff Fox <fox.geoffrey@gmail.com>
2021-01-14 13:44:42 -0800

    Conditional Rendering in JSX -  Challenge

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,40 +1,70 @@
-'use strict';
+"use strict";
 
-console.log('app.js working!');
+console.log("app.js working!");
 
 var post = {
-    title: 'The Indecision App',
-    subTitle: 'React Javascript Example'
-    //JSX - JavaScript XML
-};var template = React.createElement(
-    'div',
+  title: "The Indecision App",
+  subTitle: "React Javascript Example",
+  subTitle2: "Missing Sub-title",
+
+  options: ["one", "two"]
+};
+
+function subTitle(subTitle) {
+  if (subTitle) {
+    return React.createElement(
+      "p",
+      null,
+      subTitle
+    );
+  }
+}
+
+function options(optList) {
+  if (optList) {
+    return React.createElement(
+      "li",
+      null,
+      " Item ",
+      optList,
+      " "
+    );
+  }
+}
+
+//JSX - JavaScript XML
+var template = React.createElement(
+  "div",
+  null,
+  React.createElement(
+    "h1",
     null,
-    React.createElement(
-        'h1',
-        null,
-        post.title
-    ),
-    React.createElement(
-        'p',
-        null,
-        post.subTitle
-    ),
-    React.createElement(
-        'ol',
-        null,
-        React.createElement(
-            'li',
-            null,
-            'Item one'
-        ),
-        React.createElement(
-            'li',
-            null,
-            'Item two'
-        )
-    )
+    post.title
+  ),
+  React.createElement(
+    "p",
+    null,
+    subTitle(post.subTitle)
+  ),
+  React.createElement(
+    "p",
+    null,
+    post.options.length ? 'Here are your options:' : 'There are no options:'
+  ),
+  React.createElement(
+    "p",
+    null,
+    post.subTitle || post.subTitle2
+  ),
+  React.createElement(
+    "ol",
+    null,
+    options(post.options[0]),
+    options(post.options[1]),
+    options(post.options[2])
+  )
 );
 
-var appRoot = document.getElementById('app');
+var appRoot = document.getElementById("app");
 
 ReactDOM.render(template, appRoot);
--- src/app.js
+++ src/app.js
@@ -1,21 +1,40 @@
 console.log(`app.js working!`);
 
 var post = {
-    title: 'The Indecision App',
-    subTitle: 'React Javascript Example'
+  title: "The Indecision App",
+  subTitle: "React Javascript Example",
+  subTitle2: "Missing Sub-title",
+  
+  options: ["one", "two"],
+};
+
+function subTitle(subTitle) {
+  if (subTitle) {
+    return <p>{subTitle}</p>;
+  }
 }
+
+function options(optList) {
+  if (optList) {
+    return <li> Item {optList} </li>;
+  }
+}
+
 //JSX - JavaScript XML
 var template = (
-<div>
-   <h1>{post.title}</h1>
-   <p>{post.subTitle}</p>
-   <ol>
-       <li>Item one</li>
-       <li>Item two</li>
-   </ol>
-</div>
+  <div>
+    <h1>{post.title}</h1>
+    <p>{subTitle(post.subTitle)}</p>
+    <p>{post.options.length? 'Here are your options:' : 'There are no options:'}</p>
+    <p>{post.subTitle || post.subTitle2}</p>
+    <ol>
+      {options(post.options[0])}
+      {options(post.options[1])}
+      {options(post.options[2])}
+    </ol>
+  </div>
 );
 
-var appRoot = document.getElementById('app');
+var appRoot = document.getElementById("app");
 
 ReactDOM.render(template, appRoot);
