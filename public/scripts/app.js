34c3a3245efa481f14398bd6c756f5af4ddbb1d1
Geoff Fox <fox.geoffrey@gmail.com>
2021-01-16 14:40:30 -0800

    es6 'this' fix

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,28 +1,39 @@
-'use strict';
+"use strict";
 
 // Object literal
 var user = {
-   name: 'Andrew',
-   cities: ['Philadelphia', 'New York', 'London'],
-   printPlacesLived: function printPlacesLived() {
-      console.log(this.name);
-      console.log(this.cities);
+  name: "Andrew",
+  cities: ["Philadelphia", "New York", "London"],
+  printPlacesLived: function printPlacesLived() {
+    var _this = this;
 
-      this.cities.forEach(function (city) {
-         console.log(city); //good
-         console.log(this.name); // error
-      });
-   }
+    console.log(this.name);
+    console.log(this.cities);
+
+    // work around
+    var that = this;
+    // an arrow function in this method would cause 'this' to fail
+    this.cities.forEach(function (city) {
+      console.log(city); //good
+      // console.log(this.name); // error
+      console.log(that.name); // work around
+    });
+
+    // now for arrow functions
+    this.cities.forEach(function (city) {
+      console.log(" " + _this.name + " has lived in " + city);
+    });
+  }
 };
 
-// call function
+// call the function
 user.printPlacesLived();
 
 // JSX - JavaScript XML
 var template = React.createElement(
-   'div',
-   null,
-   'Test run'
+  "div",
+  null,
+  "Test run"
 );
 
 var appRoot = document.getElementById("app");
--- src/es6-examples.js
+++ src/es6-examples.js
@@ -1,25 +1,32 @@
 // Object literal
 const user = {
-   name: 'Andrew',
-   cities: ['Philadelphia', 'New York', 'London'],
-   printPlacesLived: function() {
-      console.log(this.name);
-      console.log(this.cities);
+  name: "Andrew",
+  cities: ["Philadelphia", "New York", "London"],
+  printPlacesLived: function () {
+    console.log(this.name);
+    console.log(this.cities);
 
-      this.cities.forEach( function(city) {
-         console.log(city); //good
-         console.log(this.name); // error
-      });
-   }
+    // work around
+    const that = this;
+    // an arrow function in this method would cause 'this' to fail
+    this.cities.forEach(function (city) {
+      console.log(city); //good
+      // console.log(this.name); // error
+      console.log(that.name); // work around
+    });
+
+    // now for arrow functions
+    this.cities.forEach((city) => {
+      console.log(` ${this.name} has lived in ${city}`);
+    });
+  },
 };
 
-// call function
+// call the function
 user.printPlacesLived();
 
 // JSX - JavaScript XML
-const template = (
-   <div>Test run</div>
-);
+const template = <div>Test run</div>;
 
 const appRoot = document.getElementById("app");
 
